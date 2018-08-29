2c33c7e8d48bbb632f324c7384ea17c3aad69000
Lukas M <lukas.mrtvy@gmail.com>
2018-08-29 17:08:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SS_SERVER=${CUSTOM_SS_SERVER:-"127.0.0.1"}
+SS_PORT=${CUSTOM_SS_PORT:-"8388"}
+SS_METHOD=${CUSTOM_SS_METHOD:-"chacha20-ietf-poly1305"}
+SS_PASS=${CUSTOM_SS_PASS:-"initialpass"}
+SS_OPTS=${CUSTOM_SS_OPTS:-"--fast-open -u"}
+
 if [ -z ${SNX_SERVER+x} ] && [ -z ${SNX_CERTPATH+x} ] && [ -z ${SNX_CERTPASS+x}];then
   echo "Not enough variables, make sure that SNX_SERVER, SNX_CERTPATH and SNX_CERTPASS is set"
   exit 1
