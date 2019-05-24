ecca9301a81e52436803382ad56a259dbd1622d8
Lukas M <lukas.mrtvy@gmail.com>
2019-05-24 20:23:20 +0200

    Update snx.sh

--- scripts/snx.sh
+++ scripts/snx.sh
@@ -2,7 +2,12 @@
 spawn snx -s $env(SNX_SERVER) -c $env(SNX_CERTPATH)
 expect "*?assword:"
 send "$env(SNX_CERTPASS)\r"
-expect "*Do you accept*"
-send "y\r"
+expect {
+  {*Do you accept*} {
+    send "y\r"
+    exp_continue
+  }
+  eof
+}
 expect "SNX - connected."
 expect eof
