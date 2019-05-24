9d40cda34a125a0528d92dc86b753aad56087334
Lukas M <lukas.mrtvy@gmail.com>
2019-05-24 22:11:35 +0200

    Update snx.sh

--- scripts/snx.sh
+++ scripts/snx.sh
@@ -1,13 +1,52 @@
-#!/usr/bin/expect -f
-spawn snx -s $env(SNX_SERVER) -c $env(SNX_CERTPATH)
-expect "*?assword:"
-send "$env(SNX_CERTPASS)\r"
+#!/usr/bin/expect
+
+set timeout 10
+log_user 1
+send_user "SNX disconnecting... if connected.\n"
+spawn snx -d
 expect {
-  {*Do you accept*} {
-    send "y\r"
-    exp_continue
-  }
-  eof
+        timeout { exit 2 }
+        eof {
+                sleep 1
+                send_user "SNX connecting...\n"
+                spawn snx -s $env(SNX_SERVER) -c $env(SNX_CERTPATH)
+                expect {
+                        "password:" {
+                                send "$env(SNX_CERTPASS)\r"
+                                expect {
+                                        "*accept*" {
+                                               send "y\r"
+                                               exp_continue
+                                        }
+                                        "connected." {
+                                                send_user "Connected!\n"
+                                                exit 0
+                                        }
+                                        "Connection aborted." {
+                                                send_user "Connection aborted\n"
+                                                exit 1
+                                        }
+                                        eof {
+                                                send_user "Error\n"
+                                                exit 1
+                                        }
+                                        timeout { 
+                                                send_user "Timeout\n"
+                                                exit 2
+                                        }
+                                }
+                        }
+                        eof {
+                                send_user "Error\n"
+                                exit 1
+                        }
+                        timeout { 
+                                send_user "Timeout\n"
+                                exit 2
+                        }
+
+                }
+    }
 }
-expect "SNX - connected."
 expect eof
+exit 1
